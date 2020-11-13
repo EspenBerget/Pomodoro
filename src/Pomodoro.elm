2b2bb9c6eeec854707fafbbb7e120afe528f84cb
EspenBerget <espen.berget@tuta.io>
2020-11-13 12:31:40 +0100

    Simple stopwatch implemented

--- src/Pomodoro.elm
+++ src/Pomodoro.elm
@@ -3,34 +3,60 @@ module Pomodoro exposing (main)
 import Browser
 import Html exposing (..)
 import Html.Events exposing (onClick)
+import Time exposing (utc, toSecond, toMinute)
+import Task
 
 -- MODEL
-type alias Model = String
+type alias Model = Maybe Int
+
+timeToString : Time.Posix -> String
+timeToString time = 
+    (String.padLeft 2 '0' <| String.fromInt <| Time.toMinute utc time) 
+    ++ ":" ++
+    (String.padLeft 2 '0' <| String.fromInt <| Time.toSecond utc time)
+
+
 -- VIEW
-view  : Model -> Html Msg
-view dummy = 
+view : Model -> Html Msg
+view model = 
     div []
-        [ h3 [] [ text dummy ]
+        [ viewTime model
         , button [ onClick Stop ] [ text "Stop" ]
         , button [ onClick Start ] [ text "Start" ]
         ]
 
+viewTime  : Model -> Html Msg
+viewTime model = 
+    case model of
+        Just time -> 
+            h3 [] [ text (timeToString <| Time.millisToPosix (time*1000)) ]
+        Nothing -> 
+            h3 [] [ text "00:00" ]
+
 -- UPDATE
 type Msg 
-    = Stop
+    = Tick Time.Posix
     | Start
+    | Stop
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = 
     case msg of 
-        Stop -> 
-            ( "stop", Cmd.none )
-        Start -> 
-            ( "start", Cmd.none )
+        Tick time -> 
+            ( Maybe.map (\x -> x+1) model, Cmd.none )
+        Stop ->
+            ( Nothing, Cmd.none )
+        Start ->
+            ( Just 0, Cmd.none ) 
+
 
 -- MAIN
 init : () -> ( Model, Cmd Msg )
-init _ =  ("No value", Cmd.none )
+init _ =  ( Nothing, Cmd.none )
+
+tick : Model -> Sub Msg
+tick model =
+    Time.every 1000 Tick
 
 main : Program () Model Msg
 main =
@@ -38,5 +64,5 @@ main =
         { init = init
         , view = view
         , update = update
-        , subscriptions = \_ -> Sub.none
+        , subscriptions = tick
         }
